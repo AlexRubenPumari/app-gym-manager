e11f57fb0679f111af11fd97919c891b5831a94d
AlexRubenPumari <alexpumari0@gmail.com>
2025-10-26 19:10:12 -0300

    refactor(domain): rewrite service in utils

--- domain/src/utils/service.ts
+++ domain/src/utils/service.ts
@@ -1,15 +1,14 @@
-import { Entity } from "./entity"
-import { NewEntity } from "./new-entity"
+import { Entity, NewEntity, UpdateEntity } from "./"
 
 export interface Service<EntityType extends Entity>
   extends QueryService<EntityType>, StorageService<EntityType> {}
 
 export interface QueryService<EntityType extends Entity> {
-  getById: (id: number) => Promise<EntityType | null>
+  getById: (entity: Entity) => Promise<EntityType | null>
 }
 
 export interface StorageService<EntityType extends Entity> {
   create: (newEntity: NewEntity<EntityType>) => Promise<EntityType>
-  update: (id: number, updateData: Partial<NewEntity<EntityType>>) => Promise<EntityType>
-  delete: (id: number) => Promise<void>
+  update: (updateEntity: UpdateEntity<EntityType>) => Promise<EntityType>
+  delete: (entity: Entity) => Promise<void>
 }
