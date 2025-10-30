adfc8d1ce0957307c73eb8e286c75b6f939b3b67
AlexRubenPumari <alexpumari0@gmail.com>
2025-10-30 12:32:13 -0300

    refactor(domain): rewrite member entity

--- domain/src/entities/member-status.ts
+++
@@ -1,6 +0,0 @@
-export const MemberStatus = {
-  ACTIVE: 'active',
-  INACTIVE: 'inactive',
-} as const
-
-export type MemberStatus = typeof MemberStatus[keyof typeof MemberStatus]
--- domain/src/entities/member.ts
+++ domain/src/entities/member.ts
@@ -1,7 +1,13 @@
-import { DateValue, Person, Entity } from "../utils"
-import { MemberStatus } from "./member-status"
+import { AbstractDate, Person, Entity } from "../utils"
+
+export const MEMBER_STATUS = {
+  ACTIVE: 'active',
+  INACTIVE: 'inactive',
+} as const
+
+type MemberStatus = typeof MEMBER_STATUS[keyof typeof MEMBER_STATUS]
 
 export interface Member extends Entity, Person {
-  registrationDate: DateValue,
+  registrationDate: AbstractDate,
   status: MemberStatus
 }
