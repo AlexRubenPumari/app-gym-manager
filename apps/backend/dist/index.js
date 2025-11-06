82aecc86fc1db93f90bcdfe60097a0d816c4c584
AlexRubenPumari <alexpumari0@gmail.com>
2025-11-05 23:09:56 -0300

    chore(backend): install express

--- apps/backend/dist/index.js
+++ apps/backend/dist/index.js
@@ -1,6 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const domain_1 = require("@gym-manager/domain");
-console.log('Test');
-console.log(domain_1.r);
+const express_1 = __importDefault(require("express"));
+const PORT = 3000;
+const app = (0, express_1.default)();
+app.get('/', (_, res) => {
+    res.send('hello world');
+});
+app.listen(PORT, () => {
+    console.log("Listening in localhost:" + PORT);
+});
 //# sourceMappingURL=index.js.map
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,2 +1,12 @@
-import { Person } from '@gym-manager/domain'
-console.log("")
+import express from "express"
+
+const PORT = 3000
+const app = express()
+
+app.get('/', (_, res) => {
+  res.send('hello world')
+})
+
+app.listen(PORT, () => {
+  console.log("Listening in localhost:" + PORT)  
+})
