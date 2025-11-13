22a3f49dad4c1c497b6c123193508ef02b38ef12
AlexRubenPumari <alexpumari0@gmail.com>
2025-11-13 19:24:40 -0300

    feat(backend): add sql server db

---
+++ apps/backend/src/db/index.ts
@@ -0,0 +1,19 @@
+import sql from "mssql"
+import { sqlConfig, sqlTestConfig } from "./config"
+
+const DATABASE_ENV = {
+  TEST: "test",
+  DEFAULT: "default",
+} as const
+
+type DatabaseEnv = typeof DATABASE_ENV[keyof typeof DATABASE_ENV]
+
+export function connectToDatabase(databaseEnv: DatabaseEnv = DATABASE_ENV.DEFAULT) {
+  const config = databaseEnv === DATABASE_ENV.TEST ? sqlTestConfig : sqlConfig
+
+  return sql.connect(config)
+    .catch(error => {
+      console.log("Error connecting to database:", error)
+      throw error
+    })
+}
