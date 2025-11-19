f9adf64727fcb91eb46037c6d581d2f319c29fbc
AlexRubenPumari <alexpumari0@gmail.com>
2025-11-19 17:00:07 -0300

    style(frontend): change button component styles

--- apps/frontend/src/components/button.tsx
+++ apps/frontend/src/components/button.tsx
@@ -10,11 +10,11 @@ interface ButtonProps {
 
 export function Button ({ variant, disabled = false, children, onClick }: ButtonProps) {
   const className = joinClasses(
-    "px-4 py-2 rounded font-bold select-none",
+    "px-4 py-2 rounded-md font-bold select-none",
     variant === "primary" && "bg-neutral-900 text-white",
-    variant === "secondary" && "border-2 border-neutral-900 text-neutral-900",
+    variant === "secondary" && "outline -outline-offset-2 outline-2 outline-neutral-900 text-neutral-900",
     variant === "primary" && disabled && "!bg-neutral-400 !text-neutral-200",
-    variant === "secondary" && disabled && "!border-neutral-400 !bg-transparent !text-neutral-400",
+    variant === "secondary" && disabled && "!outline-neutral-400 !bg-transparent !text-neutral-400",
   )
 
   return (
