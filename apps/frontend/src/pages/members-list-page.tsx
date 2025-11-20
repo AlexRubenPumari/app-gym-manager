b8c0c3e4002ed5ab63c10b30cb6c3528fdd54217
AlexRubenPumari <alexpumari0@gmail.com>
2025-11-20 16:41:29 -0300

    refactor(frontend): prepare page to be a react router page

--- apps/frontend/src/pages/members-list-page.tsx
+++ apps/frontend/src/pages/members-list-page.tsx
@@ -1,4 +1,4 @@
-import { Header, Table, Button } from "../components"
+import { Table, Button } from "../components"
 
 export function MembersListPage() {
   const tableColumns = [
@@ -13,17 +13,14 @@ export function MembersListPage() {
     { id: 3, name: "Luis", age: 32, city: "Sevilla" },
   ]
   return (
-    <div className="w-screen h-screen bg-slate-100 flex flex-col justify-start gap-4">
-      <Header user={{ name: "John Doe" }} />
-      <section className="bg-white rounded-xl shadow-md p-10 mx-6">
-        <header className="flex gap-4 mb-6">
-          <h2 className="font-bold text-xl mr-auto">Members</h2>
-          <Button variant="secondary">Edit</Button>
-          <Button variant="secondary">Delete</Button>
-          <Button variant="primary">Add</Button>
-        </header>
-        <Table columns={tableColumns} data={tableData} />
-      </section>
-    </div>
+    <section className = "bg-white rounded-xl shadow-md max-w-4xl mx-auto p-10" >
+      <header className="flex gap-4 mb-6">
+        <h2 className="font-bold text-xl mr-auto">Members</h2>
+        <Button variant="secondary">Edit</Button>
+        <Button variant="secondary">Delete</Button>
+        <Button variant="primary">Add</Button>
+      </header>
+      <Table columns={tableColumns} data={tableData} />
+    </section >
   )
 }
