c00e0dc4cd5279e853be7096d6b373de6bdcdd73
AlexRubenPumari <alexpumari0@gmail.com>
2025-11-20 16:38:22 -0300

    feat(frontend): add root page

---
+++ apps/frontend/src/pages/root-page.tsx
@@ -0,0 +1,14 @@
+import { Outlet, useNavigate } from "react-router-dom"
+import { Header } from "../components"
+
+export function RootPage() {
+  const navigate = useNavigate()
+  return (
+    <div className="w-screen h-screen bg-slate-100 flex flex-col justify-start">
+      <Header user={{ name: "John Doe" }} onLogout={() => navigate("login")} />
+      <div className="flex-1 p-6">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
