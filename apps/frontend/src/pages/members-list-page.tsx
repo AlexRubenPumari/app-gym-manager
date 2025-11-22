7d8134f5a56a580ae2ead180f6e7981c8d0b1a8c
AlexRubenPumari <alexpumari0@gmail.com>
2025-11-22 18:10:12 -0300

    refactor(frontend): rewrite logic to align with member-list page loader data

--- apps/frontend/src/pages/members-list-page.tsx
+++ apps/frontend/src/pages/members-list-page.tsx
@@ -1,26 +1,26 @@
+import { useLoaderData } from "react-router-dom"
 import { Table, Button } from "../components"
+import type { TableData } from "../components/table"
 
+const tableColumns = [
+  { key: "id", label: "Id" },
+  { key: "nationalId", label: "National id" },
+  { key: "firstName", label: "Name" },
+  { key: "lastName", label: "Last name" },
+  { key: "phone", label: "Phone" },
+]
 export function MembersListPage() {
-  const tableColumns = [
-    { key: "id", label: "Id" },
-    { key: "name", label: "Name" },
-    { key: "age", label: "Age" },
-    { key: "city", label: "City" },
-  ]
-  const tableData = [
-    { id: 1, name: "Juan", age: 28, city: "Madrid" },
-    { id: 2, name: "Ana", age: 24, city: "Barcelona" },
-    { id: 3, name: "Luis", age: 32, city: "Sevilla" },
-  ]
+  const { members } = useLoaderData() as { members: TableData[] }
+  
   return (
-    <section className = "bg-white rounded-xl shadow-md max-w-4xl mx-auto p-10" >
+    <section className="bg-white rounded-xl shadow-md max-w-4xl mx-auto p-10" >
       <header className="flex gap-4 mb-6">
         <h2 className="font-bold text-xl mr-auto">Members</h2>
         <Button variant="secondary">Edit</Button>
         <Button variant="secondary">Delete</Button>
         <Button variant="primary">Add</Button>
       </header>
-      <Table columns={tableColumns} data={tableData} />
+      <Table columns={tableColumns} data={members} />
     </section >
   )
 }
