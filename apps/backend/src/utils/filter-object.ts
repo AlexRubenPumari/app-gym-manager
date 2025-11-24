b1954f1b24ce0e8f3cc9caffa4245fb232a9619b
AlexRubenPumari <alexpumari0@gmail.com>
2025-11-24 14:05:15 -0300

    refactor(backend): improve typing in filter-object util

--- apps/backend/src/utils/filter-object.ts
+++ apps/backend/src/utils/filter-object.ts
@@ -1,10 +1,10 @@
-export function filterObject<Type>(
-  object: Record<string, Type>, predicate: (value: Type) => boolean
-) {
-  const result: Record<string, Type> = {}
+export function filterObject<Type extends Record<string, unknown>>(
+  object: Type, predicate: (value: Type[keyof Type]) => boolean
+): Partial<Type> {
+  const result: Partial<Type> = {}
 
   for (const key in object) {
-    const value = object[key]!
+    const value = object[key]
     if (predicate(value)) result[key] = value
   }
 
